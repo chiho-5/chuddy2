fee97c96e3670b8628c905b7ab6acdf56e375800
Chima Emmanuel <71467633+chiho-5@users.noreply.github.com>
2022-10-04 21:17:16 +0100

    Create index.jsx

---
+++ src/Chatbot/index.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Chatbot from "react-simple-chatbot";
+import { ThemeProvider } from "styled-components";
+
+
+const BotRedirect = ([ url, message]) => [
+  return(
+  <div>
+  <a href {url} target "_blank">
+    {messages}
+ </a>
+</div>
+); };
+
+const CHATBOT_THEME = {
+  background: "#FFFEFC",
+  headerBgColor: "#FFBFB5",
+  headerFontColor: "#fff",
+  headerFontSize: "15px",
+  botBubbleColor: "#C8D7C2",
+  botFontColor: "#fff",
+  userBubbleColot: "#FFBFB5",
+  userFontColor: "#fff"
+  },
+     
+ const ChatBottHelper = () => {
+   const steps = [
+     {
+       id: "1",
+       message: "hi, I'm chuddy",
+       trigger: "2"
+     },
+     {
+       id: "2",
+       message: "An AI assisted teacher and friend",
+       trigger: "3"
+      },
+     {
+       id: "3",
+       message: "I'll love to work with you",
+       trigger: "4"
+      },
+     {
+       id: "4",
+       options:[
+         
+       
+     
+       
+  
